fe8cb7a2c237c845f27a089f3519d340e563e11b
Pavle Jankovic <pavle.gp@gmail.com>
2019-11-07 20:41:02 +0100

    Create project

---
+++ src/main/java/rs/ac/ftn/sep/kp/KpApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.sep.kp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpApplication.class, args);
+    }
+
+}
